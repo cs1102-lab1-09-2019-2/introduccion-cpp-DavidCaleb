e8b8260e1dd8a99e1317b7a28444f97a07baf043
DavidCaleb <54277172+DavidCaleb@users.noreply.github.com>
2019-08-22 21:17:06 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-using std::cout;
+#include <cmath>
+#include <vector>
 
-int main() {
 
-    vector<int> a {1, 2, 3, 4, 5};
-    for (auto i : a) {
-        // Imprimir los numeros
+using namespace std;
+
+int main()
+{
+    int i,b;
+
+    vector<int> a = {1, 2, 3, 4, 5};
+    b=0
+
+    for (int i = a[b]; i < 6;b++) {
+        cout<<i<<'\n';
     }
+    return 0;
+    // Imprimir los numeros
+
 }
