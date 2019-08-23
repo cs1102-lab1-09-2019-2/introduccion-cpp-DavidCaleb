ce4364967b283e147992a9aaf9911091ec9964b2
DavidCaleb <54277172+DavidCaleb@users.noreply.github.com>
2019-08-22 21:15:37 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <cmath>
 using std::cout;
 
 int main() {
-    for (int i = 0; i < ; i++) {
+    for (int i = 1; i <11 ; i++) {
+        cout<<i<<"\n";
+
+
         //imprimir los 10 numeros [0-9]
     }
+    return 0;
 }
